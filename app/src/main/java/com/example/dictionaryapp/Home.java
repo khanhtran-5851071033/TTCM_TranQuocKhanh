ff32259c49e4cccb82ca72510d28b8d2fa984610
khanhtran-5851071033 <67999858+khanhtran-5851071033@users.noreply.github.com>
2020-07-14 14:33:47 +0700

    tải lên lần 10

--- app/src/main/java/com/example/dictionaryapp/Home.java
+++ app/src/main/java/com/example/dictionaryapp/Home.java
@@ -56,12 +56,34 @@ public class Home extends AppCompatActivity {
        // database.QueryData("DROP TABLE wordss");
         database.QueryData("CREATE TABLE IF NOT EXISTS Wordss(ID Integer PRIMARY KEY AUTOINCREMENT, WordName VARCHAR(50),WordSpell VARCHAR(50)" +
                 ",WordType VARCHAR(50),WordMean VARCHAR(100),WordSynonym VARCHAR(100),WordExample VARCHAR(200),favourite INT,history INT)");
-
+        //database.QueryData("DELETE FROM wordss");
+       // database.QueryData("DELETE FROM wordss");
         Cursor cursor=database.GetData("SELECT * FROM Wordss ");
         int count=cursor.getCount();
         if(count==0)
         {
             database.QueryData("INSERT INTO Wordss VALUES(null,'Hello','[helou]','Động từ','Xin chào','Hi','Hello Khánh',0,0)");
+            database.QueryData("INSERT INTO Wordss VALUES(null,'Go','/gəʊ/','Động từ','Đi','go','go to market',0,0)");
+            database.QueryData("INSERT INTO Wordss VALUES(null,'Project','/ˈprədʒekt/','Danh từ','Dự án, đề án','scheme, plan','a project to establish a new national park',0,0)");
+            database.QueryData("INSERT INTO Wordss VALUES(null,'Code','/gəʊ/','/kəʊd/','mã; mật mã; mã số','...','crack a code',0,0)");
+            database.QueryData("INSERT INTO Wordss VALUES(null,'Air','/eə[r]/ ','Danh từ','không khí, bầu không khí, không trung','air base, air beacon, air brush,','letˈs go out for some fresh air',0,0)");
+            database.QueryData("INSERT INTO Wordss VALUES(null,'Open','/ˈəʊpən/','Động từ','mở, ngỏ','go','with both eyes open',0,0)");
+            database.QueryData("INSERT INTO Wordss VALUES(null,'Because','/biˈkɒz/','Liên từ','vì, bởi vì','go','I did it because he told me',0,0)");
+            database.QueryData("INSERT INTO Wordss VALUES(null,'All','/ɔ:l/','Định từ','Tất cả','altogether, everything','all my plants have died',0,0)");
+            database.QueryData("INSERT INTO Wordss VALUES(null,'About','/əˈbaʊt/ ','Phó từ','khoảng [chừng], gần','about-face, about-sledge, about-turn','it costs about ₤10',0,0)");
+            database.QueryData("INSERT INTO Wordss VALUES(null,'Help','/help/','Động từ','giúp đỡ, cứu giúp, giúp','support','may I help with a washing-up?',0,0)");
+            database.QueryData("INSERT INTO Wordss VALUES(null,'Head','/hed/','Danh từ','Đầu','...','the ball hit her on the head',0,0)");
+            database.QueryData("INSERT INTO Wordss VALUES(null,'Never','/ˈnevə[r]/','Phó từ','không bao giờ','not ever, not once','she never goes to the cinema',0,0)");
+            database.QueryData("INSERT INTO Wordss VALUES(null,'Next','/nekst/','Tính từ','sau, tới, tiếp','next of kin, next-best','the next name on the list',0,0)");
+            database.QueryData("INSERT INTO Wordss VALUES(null,'Like','/laik/','Động từ','Thích, ưa','likeliness, likely, liken','she likes him but doesnˈt love him',0,0)");
+            database.QueryData("INSERT INTO Wordss VALUES(null,'Know','/nəʊ/','Động từ','Biết','understand','she doesnˈt know your address',0,0)");
+            database.QueryData("INSERT INTO Wordss VALUES(null,'House','/haʊs/','Danh từ','nhà; người ở trong nhà','home, abode','be quiet or you will wake the whole house',0,0)");
+            database.QueryData("INSERT INTO Wordss VALUES(null,'Document','/ˈdɒkjʊmənt/','Danh từ','Văn kiện, tư liệu','text, file','can you document these claims?',0,0)");
+            database.QueryData("INSERT INTO Wordss VALUES(null,'Support','/səˈpɔ:t/','Động từ','nâng đỡ, ủng hộ','help, prop, foundation','he was weak with hunger, so I had to support him',0,0)");
+            database.QueryData("INSERT INTO Wordss VALUES(null,'Food','/fu:d/','Danh từ','thức ăn; thực phẩm','food chain, food poisoning','we cannot survive for long without food and drink',0,0)");
+            database.QueryData("INSERT INTO Wordss VALUES(null,'Study','/ˈstʌdi/','Danh từ','Sự học hỏi, sự nghiên cứu','learning','give all oneˈs spare time to study',0,0)");
+            database.QueryData("INSERT INTO Wordss VALUES(null,'Good','/gud/','Tính từ','Tốt, hay, giỏi','better; best','the car has very good brakes',0,0)");
+            database.QueryData("INSERT INTO Wordss VALUES(null,'Practicable','/ˈpræktikəbl/','Tính từ','Làm được, khả thi','feasible,realistic','the mountain route that is practicable only in summer',0,0)");
             database.QueryData("INSERT INTO Wordss VALUES(null,'Design','/dizain/','Danh từ','Bản thiết kế, kế hoạch','Plan, scheme','The design of machine',0,0)");
             database.QueryData("INSERT INTO Wordss VALUES(null,'Information',' /ˌɪn.fəˈmeɪ.ʃən/','Danh từ','Thông tin','Info,Data',' I would like some information about your flights to the USA',0,0)");
             database.QueryData("INSERT INTO Wordss VALUES(null,'Technology','/tekˈnɒl.ə.dʒi/','Danh từ','Công nghệ, khoa học','knowledge, machinery','The technology of computers',0,0)");
--- app/src/main/java/com/example/dictionaryapp/Translate.java
+++ app/src/main/java/com/example/dictionaryapp/Translate.java
@@ -1,22 +1,41 @@
 package com.example.dictionaryapp;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.speech.RecognizerIntent;
+import android.speech.tts.TextToSpeech;
 import android.text.Editable;
 import android.text.TextWatcher;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
+import android.widget.SeekBar;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.example.model.Language;
+import com.example.model.TranslateAPI;
+
+import java.util.ArrayList;
+import java.util.Locale;
 
 public class Translate extends AppCompatActivity {
 
     EditText txt_input;
-    ImageView img_delete_input;
+    private static final int REQUEST_CODE_SPEECH_INPUT = 1;
+    ImageView img_delete_input,img_void,img_speak,img_trans;
     LinearLayout linear_output;
+    SeekBar seekBar_seed;
+    TextView txt_speed,txt_output;
     Button btn_anh_viet,btn_viet_anh;
+    private TextToSpeech mTTS;
+    private static final String TAG = "Translate";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -28,10 +47,16 @@ public class Translate extends AppCompatActivity {
 
     private void addView() {
         txt_input=findViewById(R.id.text_input);
+        txt_output=findViewById(R.id.txt_output);
         img_delete_input=findViewById(R.id.img_delete_input);
         linear_output=findViewById(R.id.linear_translate);
         btn_anh_viet=findViewById(R.id.btn_anh_viet);
         btn_viet_anh=findViewById(R.id.btn_viet_anh);
+        seekBar_seed=findViewById(R.id.mSeekBarSpeed);
+        txt_speed=findViewById(R.id.txt_speed2);
+        img_speak=findViewById(R.id.img_speak);
+        img_void=findViewById(R.id.img_void);
+        img_trans=findViewById(R.id.img_strans);
 
     }
 
@@ -62,14 +87,131 @@ public class Translate extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 linear_output.setVisibility(View.VISIBLE);
+                TranslateAPI translateAPI = new TranslateAPI(
+                        Language.AUTO_DETECT,
+                        Language.VIETNAMESE,
+                        txt_input.getText().toString());
+                translateAPI.setTranslateListener(new TranslateAPI.TranslateListener() {
+                    @Override
+                    public void onSuccess(String translatedText) {
+                        Log.d(TAG, "onSuccess: "+translatedText);
+                        txt_output.setText(translatedText);
+                    }
+
+                    @Override
+                    public void onFailure(String ErrorText) {
+                        Log.d(TAG, "onFailure: "+ErrorText);
+
+                    }
+                });
             }
         });
         btn_viet_anh.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 linear_output.setVisibility(View.VISIBLE);
+                TranslateAPI translateAPI = new TranslateAPI(
+                        Language.AUTO_DETECT,
+                        Language.ENGLISH,
+                        txt_input.getText().toString());
+                translateAPI.setTranslateListener(new TranslateAPI.TranslateListener() {
+                    @Override
+                    public void onSuccess(String translatedText) {
+                        Log.d(TAG, "onSuccess: "+translatedText);
+                        txt_output.setText(translatedText);
+                    }
+
+                    @Override
+                    public void onFailure(String ErrorText) {
+                        Log.d(TAG, "onFailure: "+ErrorText);
+
+                    }
+                });
+            }
+        });
+        seekBar_seed.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+                txt_speed.setText(progress+"%"); }
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) { }
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) { }
+        });
+        img_void.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                speak();
+            }
+        });
+        img_speak.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                texttoSpeak();
+            }
+        });
+        img_trans.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String tam=txt_input.getText().toString();
+                String tam1=txt_output.getText().toString();
+                txt_output.setText(tam);
+                txt_input.setText(tam1);
             }
         });
 
+    }
+    private void speak(){
+        Intent intent=new Intent(RecognizerIntent.ACTION_RECOGNIZE_SPEECH);
+        intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE_MODEL,RecognizerIntent.LANGUAGE_MODEL_FREE_FORM);
+        intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE, Locale.ENGLISH);
+        intent.putExtra(RecognizerIntent.EXTRA_PROMPT,"Hi speaking something");
+        try {
+            startActivityForResult(intent,REQUEST_CODE_SPEECH_INPUT);
+        }
+        catch (Exception e)
+        {
+            Toast.makeText(this,""+e.getMessage(),Toast.LENGTH_LONG).show();
+        }
+    }
+    private void texttoSpeak() {
+        mTTS = new TextToSpeech(this, new TextToSpeech.OnInitListener() {
+            @Override
+            public void onInit(int status) {
+                if (status == TextToSpeech.SUCCESS) {
+                    int result = mTTS.setLanguage(Locale.ENGLISH);
+                    if (result == TextToSpeech.LANG_MISSING_DATA || result == TextToSpeech.LANG_NOT_SUPPORTED) {
+                        Log.e("TTS", "Language not supported");
+                    } else {
+                    }
+                } else {
+                    Log.e("TTS", "Initialization failed");
+                }
+                float speed = (float) seekBar_seed.getProgress() / 50;
+                if (speed < 0.1) speed = 0.1f;
+                mTTS.setSpeechRate(speed);
+                mTTS.speak(txt_input.getText().toString(), TextToSpeech.QUEUE_FLUSH, null);
+            }
+        });
+    }
+
+    @Override
+    protected void onDestroy() {
+        if(mTTS!=null)
+        {
+            mTTS.stop();
+            mTTS.shutdown();
+        }
+        super.onDestroy();
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        if(resultCode==RESULT_OK && requestCode==REQUEST_CODE_SPEECH_INPUT)
+        {
+            ArrayList<String> resutl=data.getStringArrayListExtra(RecognizerIntent.EXTRA_RESULTS);
+            txt_input.setText(resutl.get(0));
+        }
+        super.onActivityResult(requestCode, resultCode, data);
     }
 }
